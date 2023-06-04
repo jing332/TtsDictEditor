23ca10000c7480e3b27e342d96b01dbb751c3e84
Jing <42014615+jing332@users.noreply.github.com>
2023-06-04 17:04:57 +0800

    refactor: UI

--- app/src/main/java/com/github/jing332/tts_dict_editor/ui/AboutScreen.kt
+++ app/src/main/java/com/github/jing332/tts_dict_editor/ui/AboutScreen.kt
@@ -1,5 +1,8 @@
 package com.github.jing332.tts_dict_editor.ui
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -11,14 +14,20 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
 import com.github.jing332.tts_dict_editor.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen() {
     val navController = LocalNavController.current
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(stringResource(R.string.about)) }, navigationIcon = {
@@ -31,7 +40,20 @@ fun AboutScreen() {
         }
     ) {
         Surface(modifier = Modifier.padding(it)) {
-            Text("About")
+            val url = remember { "https://github.com/jing332/TtsDictEditor" }
+            Text(
+                "项目开源地址：$url",
+                color = Color.Blue,
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.padding(16.dp).clickable {
+                    context.startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse(url)
+                        )
+                    )
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/github/jing332/tts_dict_editor/ui/MainScreen.kt
+++ app/src/main/java/com/github/jing332/tts_dict_editor/ui/MainScreen.kt
@@ -1,13 +1,15 @@
 package com.github.jing332.tts_dict_editor.ui
 
-import android.app.Activity
 import android.os.Bundle
-import android.os.Parcelable
 import android.os.SystemClock
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -23,6 +25,8 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
@@ -30,7 +34,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
@@ -42,13 +45,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.NavDestination
@@ -57,11 +62,12 @@ import androidx.navigation.NavOptions
 import androidx.navigation.Navigator
 import com.github.jing332.tts_dict_editor.BuildConfig
 import com.github.jing332.tts_dict_editor.R
-import com.github.jing332.tts_dict_editor.app
 import com.github.jing332.tts_dict_editor.ui.home.HomeScreen
 import com.talhafaki.composablesweettoast.util.SweetToastUtil
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Composable
 fun MainScreen(
@@ -120,6 +126,7 @@ fun MainScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun DrawerContent(modifier: Modifier, navController: NavHostController) {
     val drawerItemIcon = @Composable { img: ImageVector, contentDescription: String ->
@@ -142,27 +149,60 @@ private fun DrawerContent(modifier: Modifier, navController: NavHostController) 
 
     Column(modifier = modifier.verticalScroll(rememberScrollState())) {
         Spacer(modifier = Modifier.height(24.dp))
+        val context = LocalContext.current
+        val clipboardManager = LocalClipboardManager.current
+        val snackBarState = LocalSnackbarHostState.current
+        val scope = rememberCoroutineScope()
 
-        Row {
-            Image(
-                painterResource(id = R.drawable.ic_new_launcher_foreground),
-                stringResource(id = R.string.app_name),
-                modifier = Modifier.size(64.dp)
-            )
-            Column(modifier = Modifier.align(Alignment.CenterVertically)) {
-                Text(
-                    text = stringResource(id = R.string.app_name),
-                    style = MaterialTheme.typography.titleMedium
+        var isBuildTimeExpanded by remember { mutableStateOf(false) }
+        val versionNameText = remember {
+            "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+        }
+        Column(modifier = Modifier
+            .padding(end = 4.dp)
+            .clip(MaterialTheme.shapes.small)
+            .combinedClickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(bounded = true),
+                onClick = {
+                    isBuildTimeExpanded = !isBuildTimeExpanded
+                },
+                onLongClick = {
+                    clipboardManager.setText(AnnotatedString(versionNameText))
+                    scope.launch {
+                        withTimeout(2000) {
+                            snackBarState.showSnackbar(context.getString(R.string.copied_to_clipboard))
+                        }
+                    }
+                }
+            )) {
+            Row {
+                Image(
+                    painterResource(id = R.drawable.ic_new_launcher_foreground),
+                    stringResource(id = R.string.app_name),
+                    modifier = Modifier.size(64.dp)
                 )
-                SelectionContainer {
+                Column(modifier = Modifier.align(Alignment.CenterVertically)) {
                     Text(
-                        text = "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})",
+                        text = stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = versionNameText,
                         style = MaterialTheme.typography.bodyMedium
                     )
                 }
             }
+            AnimatedVisibility(visible = isBuildTimeExpanded) {
+                Text(
+                    text = "构建于 " + SimpleDateFormat(
+                        "yyyy-MM-dd HH:mm:ss",
+                        Locale.getDefault()
+                    ).format(BuildConfig.BUILD_TIME * 1000),
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
         }
-
         Divider(
             modifier = Modifier
                 .fillMaxWidth()
