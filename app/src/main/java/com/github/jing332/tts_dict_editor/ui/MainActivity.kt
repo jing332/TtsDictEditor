cff136a08f11e1f3db5cde0520e077f08eb200d3
Jing <42014615+jing332@users.noreply.github.com>
2023-06-04 10:46:34 +0800

    feat: 返回处理

--- app/src/main/java/com/github/jing332/tts_dict_editor/ui/MainActivity.kt
+++ app/src/main/java/com/github/jing332/tts_dict_editor/ui/MainActivity.kt
@@ -2,18 +2,32 @@ package com.github.jing332.tts_dict_editor.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -28,9 +42,14 @@ import com.github.jing332.tts_dict_editor.ui.home.DictFileParamType
 import com.github.jing332.tts_dict_editor.ui.theme.AppTheme
 import com.github.jing332.tts_dict_editor.ui.widget.Widgets.TransparentSystemBars
 import com.github.jing332.tts_server_android.util.longToast
+import com.talhafaki.composablesweettoast.util.SweetToastUtil
+import kotlinx.coroutines.launch
 
 val LocalNavController = staticCompositionLocalOf<NavHostController> {
     error("NavController has not been initialized! ")
+}
+val LocalSnackbarHostState = staticCompositionLocalOf<SnackbarHostState> {
+    error("SnackbarHostState has not been initialized! ")
 }
 
 class MainActivity : ComponentActivity() {
@@ -83,7 +102,38 @@ class MainActivity : ComponentActivity() {
         navController: NavHostController = rememberNavController(),
         drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
     ) {
-        CompositionLocalProvider(LocalNavController provides navController) {
+        val snackbarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+
+        var lastBackDownTime by remember { mutableLongStateOf(0L) }
+        var toastMsg by remember { mutableStateOf("") }
+        if (toastMsg.isNotEmpty()) {
+            SweetToastUtil.SweetInfo(
+                message = toastMsg,
+                Toast.LENGTH_SHORT,
+                PaddingValues(bottom = 32.dp)
+            )
+            toastMsg = ""
+        }
+
+        BackHandler {
+            SystemClock.elapsedRealtime().let {
+                if (it - lastBackDownTime <= 1500) {
+                    finish()
+                } else {
+                    lastBackDownTime = it
+                    scope.launch {
+                        snackbarHostState.showSnackbar("")
+                    }
+                    toastMsg = "再按一次退出程序"
+                }
+            }
+        }
+
+        CompositionLocalProvider(
+            LocalNavController provides navController,
+            LocalSnackbarHostState provides snackbarHostState
+        ) {
             NavHost(
                 navController,
                 startDestination = AppNavRoutes.DictFileManager.route
@@ -109,6 +159,9 @@ class MainActivity : ComponentActivity() {
                 composable(AppNavRoutes.About.route) {
                     AboutScreen()
                 }
+            }
+            SnackbarHost(hostState = snackbarHostState) {
+                Text(it.visuals.message)
             }
         }
     }
